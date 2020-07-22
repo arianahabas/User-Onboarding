b3bd65cdd65062598acc90677cb9084dc76803dc
arianahabas <arianahabas@gmail.com>
2020-07-22 18:11:28 -0500

    updated files

---
+++ src/components/App.js
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from 'react'
+import Players from './Players'
+import PlayerForm from './PlayerForm'
+import styled from 'styled-components'
+
+import formSchema from '../validation/formSchema'
+import axios from 'axios'
+import * as yup from 'yup'
+
+//STYLES//
+const AppStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;    
+`
+const FormStyles= styled.div`
+  border: 3px solid #EEC643;
+  color: #EEF0F2;
+  background-color: #0D21A1;
+  padding: 50px;
+  margin: 10px;
+  border-radius: 20px;
+  width: 60vw;
+  text-align: center;
+`
+const PlayerCard= styled.div`
+  margin: 20px; 
+  border: 4px dashed  #EEF0F2;
+`
+//INTITIAL STATES//
+const initialPlayersList = []
+const initialDisabled = true
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  name: '',
+  email:'',
+  number: '',
+  password: '',
+  ///// CHECKBOX /////
+  position: {
+    Guard: false,
+    Forward: false,
+    Center: false,
+  },
+}
+const initialFormErrors = {
+  name: '',
+  email: '',
+  number: '',
+  password: '',
+}
+
+export default function App () {
+  //STATES//
+  const [players, setPlayers] = useState(initialPlayersList)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)    
+  
+  //HELPER FUNCTIONS//
+  const getPlayers = () => {
+    axios.get('https://reqres.in/api/users')
+    .then(res => {
+      setPlayers(res.data)
+    })
+    .catch(err =>{
+      debugger
+    })
+  }
+
+  const postNewPlayer = newPlayer => {
+    axios.post('https://reqres.in/api/users', newPlayer)
+    .then(res => {
+      setPlayers([res.data, ...players])
+      setFormValues(initialFormValues)
+    })
+    .catch(err =>{
+      debugger
+    })
+  }
+  //FORM ACTIONS//
+  const inputChange = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+          .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        })
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        })
+      })
+      setFormValues({
+        ...formValues,
+        [name]: value 
+      })
+    }
+    const checkboxChange = (name, isChecked) => {
+
+      setFormValues({
+        ...formValues,
+        position: {
+          ...formValues.position,
+          [name]: isChecked, 
+        }
+      })
+    }
+    const submit = () => {
+      const newPlayer = {
+        name: formValues.name.trim(),
+        email: formValues.email.trim(),
+        number: formValues.number.trim(),
+        password: formValues.password.trim(),
+        position: Object.keys(formValues.position).filter(p => formValues.position[p]),
+      }
+      postNewPlayer(newPlayer)
+    }
+    //SIDE EFFECTS//
+    useEffect(() => {
+      getPlayers()
+    }, [])
+  
+    useEffect(() => {
+      formSchema.isValid(formValues).then(valid => {
+        setDisabled(!valid)
+      })
+    }, [formValues])
+
+  return(
+    <AppStyles className='App'>
+      <FormStyles className='form'>
+        <PlayerForm
+        values={formValues}
+        inputChange={inputChange}
+        checkboxChange={checkboxChange}
+        submit={submit}
+        disabled={disabled}
+        errors={formErrors}
+        />
+    </FormStyles>
+    <PlayerCard className='playerCard'>
+      {
+        players.map(player => {
+          return (
+            <Players key={player.id} details={player} />
+          )
+        })
+      }
+      </PlayerCard>
+    </AppStyles>
+  )
+}
+
---
+++ src/components/PlayerForm.js
@@ -0,0 +1,118 @@
+import React from 'react'
+
+export default function PlayerForm (props) {
+    const { 
+      values, 
+      submit,
+      inputChange,
+      checkboxChange,
+      disabled,
+      errors, 
+    } = props 
+
+    const onInputChange = evt => {
+      const { name, value } = evt.target
+      inputChange(name, value)
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+      }
+    const onCheckboxChange = evt => {
+      const { name, checked } = evt.target
+      checkboxChange(name, checked)
+    }
+
+    return (
+        <div>
+        <form onSubmit = {onSubmit}>
+            <h1>Join the Team! 🏀</h1>
+          <label htmlFor='nameInput'></label>
+          <input
+          id='nameInput'
+          name='name'
+          type='text'
+          maxLength='20'
+          placeholder='Full Name'
+          value={values.name}
+          onChange={onInputChange}
+          />
+          <br/>
+          <label htmlFor='emailInput'></label>
+          <input
+          id='emailInput'
+          name='email'
+          type='email'
+          maxLength='20'
+          placeholder='Enter Email'
+          value={values.email}
+          onChange={onInputChange}
+          />
+          <br/>
+          <label htmlFor='numberInput'></label>
+          <input
+          id='numberInput'
+          name='number'
+          type='number'
+          maxLength='20'
+          placeholder='Jersey Number'
+          value={values.number}
+          onChange={onInputChange}
+          />
+          <br/>
+          <label htmlFor='passwordInput'></label>
+          <input
+          id='passwordInput'
+          name='password'
+          type='password'
+          maxLength='20'
+          placeholder='Enter Password'
+          value={values.password}
+          onChange={onInputChange}
+          />
+          <br/>
+    
+          <div className='form-group checkboxes'>
+            <h3>Desired Positions</h3>
+
+            <label>Forward
+              <input
+                type='checkbox'
+                name='forward'
+                checked={values.position.forward === true}
+                onChange={onCheckboxChange}
+                />
+            </label>
+
+            <label>Guard
+              <input
+                type='checkbox'
+                name='guard'
+                checked={values.position.guard === true}
+                onChange={onCheckboxChange}
+                />
+            </label>
+
+            <label>Center
+              <input
+                type='checkbox'
+                name='center'
+                checked={values.position.center === true}
+                onChange={onCheckboxChange}
+                />
+            </label>
+          </div>
+          <br/>
+          <button disabled={disabled}>Lets GO!!!</button> 
+          <div className='errors'>
+            <div>{errors.name}</div>
+            <div>{errors.email}</div>
+            <div>{errors.number}</div>
+            <div>{errors.position}</div>
+          </div>
+        </form>
+        </div>
+    )
+}
+
